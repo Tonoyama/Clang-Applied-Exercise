b82c0c1eb6f5935d7d411c2703b4ff12365b6c2f
Tonoyama <yudaitonoyama@gmail.com>
2021-04-12 20:10:21 +0900

    root N に計算量を減らしました

--- 2-first-2/P02-A_7.cpp
+++ 2-first-2/P02-A_7.cpp
@@ -1,25 +1,30 @@
 /**
-*    author:  Tonoyama
-*    created: 12.04.2021 14:01:05
-**/
+ *    author:  Tonoyama
+ *    created: 12.04.2021 14:01:05
+ **/
 
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #define NUM 100
 
-int main()
-{
-    int i;
- 
+void Divisors(int n) {
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            if (n / i == i)
+                printf("%d ", i);
+            else
+                printf("%d %d ", i, n / i);
+        }
+    }
+}
+
+int main() {
     int num;
- 
+
     printf("input n => ");
     scanf("%d",&num);
- 
-    for( i=1; i<=num; ++i ){
-        if( num%i == 0 )
-        printf("%d ", i);
-    }
- 
+
+    Divisors(num);
     return 0;
 }
